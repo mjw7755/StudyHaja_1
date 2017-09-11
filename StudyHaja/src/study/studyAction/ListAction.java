6397b02559099470413c9d6b286c1b0df09950cc
LeeRam <LeeRam@192.168.0.141>
2017-09-11 18:49:04 +0900

    as

---
+++ StudyHaja/src/study/studyAction/ListAction.java
@@ -0,0 +1,27 @@
+package study.studyAction;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import studyModel.NoticeDAO;
+import studyModel.NoticeVO;
+
+public class ListAction implements CommandAction {
+
+	@Override
+	public String process(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		int pageSize = 5;
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-mm-dd");
+		ArrayList<NoticeVO> list = new ArrayList<NoticeVO>();
+		NoticeDAO dao = NoticeDAO.getInstance();
+		list = dao.listAll();
+		
+		request.setAttribute("list", list);
+		System.out.println(list.get(0).getSubject());
+		return "/jsp/notice_view.jsp";
+	}
+
+}
