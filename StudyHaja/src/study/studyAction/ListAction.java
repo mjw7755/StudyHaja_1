6153873d8671e531e69246534cbcd4f429408069
LeeRam <LeeRam@192.168.0.141>
2017-09-19 10:21:37 +0900

    asd

--- StudyHaja/src/study/studyAction/ListAction.java
+++ StudyHaja/src/study/studyAction/ListAction.java
@@ -1,27 +1,44 @@
 package study.studyAction;
 
+import java.sql.Timestamp;
 import java.text.SimpleDateFormat;
-
 import java.util.ArrayList;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import studyModel.NoticeDAO;
-import studyModel.NoticeVO;
+import edu.kosta.Model.NoticeDAO;
+import edu.kosta.Model.NoticeVO;
 
 public class ListAction implements CommandAction {
 
 	@Override
 	public String process(HttpServletRequest request, HttpServletResponse response) throws Exception {
 		int pageSize = 5;
-		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-mm-dd");
+		/*SimpleDateFormat sdf = new SimpleDateFormat("yy-MM-dd");*/
+		
+		String pageNum = request.getParameter("pageNum");
+		if(pageNum == null) pageNum="1";
+		
+		int currentPage = Integer.parseInt(pageNum);
+		int count = 0, number=0;
+		
+		number = count - (currentPage - 1) * pageSize;
+		
 		ArrayList<NoticeVO> list = new ArrayList<NoticeVO>();
 		NoticeDAO dao = NoticeDAO.getInstance();
+		NoticeVO vo = new NoticeVO();
 		list = dao.listAll();
 		
+		request.setAttribute("currentPage", new Integer(currentPage));
+		request.setAttribute("count", new Integer(count));
+		request.setAttribute("pageSize", new Integer(pageSize));
+		request.setAttribute("number", new Integer(number));
 		request.setAttribute("list", list);
-		return "/ram/jsp/notice_view.jsp";
+		/*request.setAttribute("sdf", sdf);*/
+		System.out.println(list.get(0).getSubject());
+		return "/jsp/main_notice.jsp";
 	}
 
-}
+	
+	}
--- StudyHaja/src/studyModel/NoticeDAO.java
+++ StudyHaja/src/studyModel/NoticeDAO.java
@@ -25,10 +25,13 @@ public class NoticeDAO {
 	
 	public NoticeDAO(){	}
 	
-	public Connection getConnection() throws Exception {
+	public Connection getConnection() throws Exception{
 		Context ctx = new InitialContext();
-//		Context env = (Context)ctx.lookup("java:comp/env");
-		DataSource ds = (DataSource)ctx.lookup("java:comp/env/jdbc:BoardDB");
+		
+		DataSource ds = (DataSource)ctx.lookup("java:comp/env/jdbc:NoticeDB");
+		
+		DriverManager.setLogWriter(new PrintWriter(System.out));
+		
 		return ds.getConnection();
 	}
 	
@@ -58,13 +61,13 @@ public class NoticeDAO {
 		Connection conn = null;
 		PreparedStatement pstmt = null;
 		ResultSet rs = null;
-		ArrayList<NoticeVO> list = null;
+		ArrayList list = null;
 		
 		try {
 			conn = getConnection();
 			StringBuffer sb = new StringBuffer();
 			
-			sb.append("SELECT SUBJECT, REG_DATE FROM NOTICE");
+			sb.append("select subject, reg_date,num from notice order by reg_date desc");
 			
 			pstmt = conn.prepareStatement(sb.toString());
 			rs = pstmt.executeQuery();
@@ -72,11 +75,9 @@ public class NoticeDAO {
 			NoticeVO vo = null;
 			while(rs.next()){
 				vo = new NoticeVO();
-				vo.setSubject(rs.getString("SUBJECT"));
-				vo.setReg_date(rs.getTimestamp("REG_DATE"));
-				/*System.out.println(rs.getString("subject"));
-				System.out.println(rs.getTimestamp("reg_date"));*/
-
+				vo.setSubject(rs.getString("subject"));
+				vo.setReg_date(rs.getTimestamp("reg_date"));
+				vo.setNum(rs.getInt("num"));
 				list.add(vo);
 			}
 			
@@ -85,9 +86,174 @@ public class NoticeDAO {
 		} finally{
 			CloseUtil.close(rs); CloseUtil.close(pstmt); CloseUtil.close(conn);
 		}
-		
 		return list;
 	}
 	
+	public int insert(NoticeVO vo){
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs =  null;
+		
+		int num = vo.getNum();
+		int number = 0;
+		StringBuffer sb = new StringBuffer();
+		
+		try {
+			conn = getConnection();
+			
+			pstmt = conn.prepareStatement("select max(num) from notice");
+			rs= pstmt.executeQuery();
+			
+			if(rs.next()){
+				number = rs.getInt(1) + 1;
+			}else {
+				number = 1;
+			}
+			
+			sb.append("insert into notice(num, writer, subject, content, reg_date) values(notice_num.nextval, ?, ?, ?, ?)");
+			
+			pstmt = conn.prepareStatement(sb.toString());
+			pstmt.setString(1, vo.getWriter());
+			pstmt.setString(2, vo.getSubject());
+			pstmt.setString(3, vo.getContent());
+			pstmt.setTimestamp(4, vo.getReg_date());
+			
+			pstmt.executeUpdate();
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally{
+			CloseUtil.close(rs); CloseUtil.close(pstmt); CloseUtil.close(conn);
+		}
+		return 0;
+	}
 	
+	public NoticeVO getDataDetail(int num){
+		Connection conn= null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		
+		NoticeVO vo = null;
+		String sql ="";
+			
+			try {
+				conn = getConnection();
+				
+				sql = "update notice set readcount = readcount + 1 where num=?";
+				pstmt = conn.prepareStatement(sql);
+				pstmt.setInt(1, num);
+				pstmt.executeQuery();
+				
+				pstmt = conn.prepareStatement("select * from notice where num=?");
+				pstmt.setInt(1, num);
+				rs = pstmt.executeQuery();
+		
+				if(rs.next()){
+					vo = new NoticeVO();
+					vo.setNum(rs.getInt("num"));
+					vo.setWriter(rs.getString("writer"));
+					vo.setSubject(rs.getString("subject"));
+					vo.setReg_date(rs.getTimestamp("reg_date"));
+					vo.setReadcount(rs.getInt("readcount"));
+					vo.setContent(rs.getString("content"));
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			} finally{
+				CloseUtil.close(rs); CloseUtil.close(pstmt);
+			}
+			
+			return vo;
+	}
+	
+	public NoticeVO update(int num){
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs =null;
+		NoticeVO vo = null;
+		
+		try {
+			conn = getConnection();
+			pstmt = conn.prepareStatement("select * from notice where num = ?");
+			pstmt.setInt(1, num);
+			rs = pstmt.executeQuery();
+			
+			if(rs.next()){
+				vo = new NoticeVO();
+				vo.setNum(rs.getInt("num"));
+				vo.setWriter(rs.getString("writer"));
+				vo.setSubject(rs.getString("subject"));
+				vo.setPasswd(rs.getString("passwd"));
+				vo.setReg_date(rs.getTimestamp("reg_date"));
+				vo.setReadcount(rs.getInt("readcount"));
+				vo.setContent(rs.getString("content"));
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally{
+			CloseUtil.close(rs); CloseUtil.close(pstmt); CloseUtil.close(conn);
+		}
+		return vo;
+	}
+	
+	public int update(NoticeVO vo){
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		
+		String sql = "";
+		int result = -1;
+		
+		try {
+			conn = getConnection();
+			pstmt= conn.prepareStatement("select * from notice where num = ?");
+			pstmt.setInt(1, vo.getNum());
+			rs = pstmt.executeQuery();
+			
+			sql = "update notice set subject=? ";
+					sql += " , content=? where num= ?";
+					
+					System.out.println(sql);
+					
+					pstmt = conn.prepareStatement(sql);
+					
+					pstmt.setString(1, vo.getSubject());
+					pstmt.setString(2, vo.getContent());
+					pstmt.setInt(3, vo.getNum());
+					
+					pstmt.executeUpdate();
+					result= 1;
+				
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally{
+			CloseUtil.close(rs); CloseUtil.close(pstmt); CloseUtil.close(conn);
+		}
+		return result;
+	}
+	
+	public int delete(int num){
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		System.out.println("되니?");
+		int result = 0;
+		try {
+			conn = getConnection();
+			pstmt = conn.prepareStatement("select * from notice where num=?");
+			pstmt.setInt(1, num);
+			rs = pstmt.executeQuery();
+			
+			if(rs.next()){
+				pstmt = conn.prepareStatement("delete from notice where num = ?");
+				pstmt.setInt(1, num);
+				result = pstmt.executeUpdate();
+				result = 1;
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally{
+			CloseUtil.close(rs); CloseUtil.close(pstmt); CloseUtil.close(conn);
+		}
+		return result;
+	}
 }
