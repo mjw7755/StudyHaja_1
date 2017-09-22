22fab3d8a024fd0f6d77b85eabc5d05a995c27c8
mjw <mjw7755@naver.com>
2017-09-22 15:58:08 +0900

    dgfgfg

--- StudyHaja/WebContent/db/reple.sql
+++ StudyHaja/WebContent/db/reple.sql
@@ -1,15 +1,25 @@
 create table Reply (
+	num number(10) not null,
+	reply_num number(10) not null,
 	id varchar2(20) not null,
 	content  nvarchar2(2000) not null,
 	reg_date date,
 	star number(10)
 )SEGMENT creation IMMEDIATE;
 
+
+drop sequence reply_num;
+create sequence reply_num
+start with 1
+increment BY 1
+maxvalue 999;
+
+
 select * from Reply;
 
 insert into Reply(id,content) values('mjw','하지마루욧');
 
-create sequence reply_num;
+
 
 delete from Reply;
 
