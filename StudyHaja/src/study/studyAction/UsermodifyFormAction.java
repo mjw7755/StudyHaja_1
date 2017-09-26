979f173787f3df784db98435f98e61275c547063
AHN <AHN@192.168.0.179>
2017-09-26 11:01:40 +0900

    sadasdasd

--- StudyHaja/src/study/studyAction/UsermodifyFormAction.java
+++ StudyHaja/src/study/studyAction/UsermodifyFormAction.java
@@ -6,21 +6,24 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-import StudyJoinModel.MemberJoinVO;
 import StudyJoinModel.memberJoinDAO;
-import edu.kosta.roomModel.roomDAO;
 
-public class MemberModifyFormAction implements CommandAction {
+public class UsermodifyFormAction implements CommandAction {
 
 	@Override
 	public String process(HttpServletRequest request, HttpServletResponse response) throws Exception {
+
+		System.out.println("connection success1");
 		
 		request.setCharacterEncoding("utf-8");
+		
 		HttpSession session = request.getSession();
 		List list=null;
 		memberJoinDAO memberdao =  memberJoinDAO.getInstance();
+		System.out.println("connection success2");
 		
 		list = memberdao.modifySelectAll(session.getAttribute("sessionid").toString());
+		
 		request.setAttribute("list", list);
 		
 		return "/ahn/jsp/memModifyForm.jsp";
